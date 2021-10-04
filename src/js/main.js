018aab7b0fbda5bd0a07941cc1364e055327b1a1
byCRXHIT <70042525+byCRXHIT@users.noreply.github.com>
2021-10-04 06:44:21 +0200

    Uploaded all Files

---
+++ src/js/main.js
@@ -0,0 +1,121 @@
+import { PasswordGenerator } from './generatepassword';
+
+let currentPassword = [];
+let clipboardState = false;
+const controlsContainer = document.querySelector('#controls');
+const passwordContainer = document.querySelector('#password');
+const lengthContainer = document.querySelector('#length-value');
+const passwordGenerator = new PasswordGenerator();
+const getRandomCharacter = () => {
+    return Math.random().toString(16).substr(2, 1);
+}
+
+const getRandomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+//⌄⌄⌄ Real ebig stuff here ⌄⌄⌄
+const animatePassword = () => {
+    passwordContainer.innerHTML = '';
+    const passwordElements = document.createElement('div');
+    passwordElements.setAttribute('class', 'characters');
+    currentPassword.forEach((character, index) => {
+        const characterElement = document.createElement('div');
+        characterElement.setAttribute('class', 'character');
+        characterElement.setAttribute('id', `character-${index}`);
+        characterElement.setAttribute('data-character', character);
+        passwordElements.appendChild(characterElement);
+    });
+    passwordContainer.appendChild(passwordElements);
+    const copyed = document.createElement('div');
+    copyed.innerHTML = 'Copy Password';
+    copyed.setAttribute('id', 'copyed');
+    copyed.setAttribute('class', 'copyed');
+    passwordContainer.appendChild(copyed);
+    const characterContainers = document.querySelectorAll('.character');
+    characterContainers.forEach(character => {
+        animateCharacter(character)
+    });
+}
+
+const animateCharacter = (element) => {
+    setTimeout(() => {
+        element.classList.add('show');
+        setTimeout(() => {
+            element.classList.add('animate');
+            const maxTicker = getRandomNumber(5, 15);
+            let ticker = 0;
+            const letterAnimation = setInterval(() => {
+                element.innerHTML = getRandomCharacter(10);
+                if (ticker === maxTicker) {
+                    element.classList.remove('animate');
+                    element.classList.add('done');
+                    element.innerHTML = element.dataset.character;
+                    clearInterval(letterAnimation);
+                }
+                ticker++;
+            }, 50);
+        }, getRandomNumber(0, 150));
+    }, getRandomNumber(0, 150));
+}
+
+const copyToClipBoard = () => {
+    if (!clipboardState) {
+        clipboardState = true;
+        navigator.clipboard.writeText(currentPassword.join('')).then(() => {
+            const cta = document.querySelector('#copyed');
+            cta.classList.add('copyed-done');
+            cta.innerHTML = 'Copied Password to Clipboard!';
+            setTimeout(() => {
+                cta.classList.remove('copyed-done');
+                clipboardState = false;
+            }, 500);
+        });
+    }
+}
+
+const setCurrentPassword = (controls) => {
+    currentPassword = passwordGenerator.getPassword(controls);
+    animatePassword();
+}
+const getFormValues = ($event) => {
+    let controlValues = {};
+    Object.keys($event.currentTarget.elements).forEach(key => {
+        let element = $event.currentTarget.elements[key];
+        if (element.type === 'checkbox') {
+            controlValues[element.name] = element.checked;
+        }
+        
+        if (element.type === 'range') {
+            controlValues[element.name] = +element.value;
+            lengthContainer.innerHTML = +element.value;
+        }
+    });
+    return controlValues;
+}
+
+setCurrentPassword({
+    length: 45,
+    specialCharacters: true,
+    numbers: true,
+    letters: true,
+    lockedSpecialCharacters: false
+});
+
+
+passwordContainer.addEventListener('click', copyToClipBoard);
+
+controlsContainer.addEventListener('submit', ($event) => {
+    $event.preventDefault();
+    setCurrentPassword(getFormValues($event));
+});
+
+controlsContainer.addEventListener('change', ($event) => {
+    $event.preventDefault();
+    setCurrentPassword(getFormValues($event));
+});
+
+controlsContainer.addEventListener('input', ($event) => {
+    $event.preventDefault();
+    getFormValues($event);
+});
